2ed797f4561847515e400dc111b2c3c9c01dba0f
daveinc <118027054+daveinc@users.noreply.github.com>
2026-03-22 00:59:11 +0200

    Create exceptions.py

---
+++ custom_components/provision_isr/provision_api/exceptions.py
@@ -0,0 +1,29 @@
+"""Exceptions for Provision ISR API client."""
+
+
+class ProvisionError(Exception):
+    """Base exception for Provision ISR API errors."""
+
+
+class ConnectionError(ProvisionError):
+    """Connection to device failed."""
+
+
+class AuthenticationError(ProvisionError):
+    """Authentication failed."""
+
+
+class InvalidRequestError(ProvisionError):
+    """Invalid request URL or parameters (Error Code 1)."""
+
+
+class InvalidXMLFormatError(ProvisionError):
+    """Invalid XML format (Error Code 2)."""
+
+
+class InvalidXMLContentError(ProvisionError):
+    """Invalid XML content or out-of-range parameters (Error Code 3)."""
+
+
+class PermissionDeniedError(ProvisionError):
+    """Permission denied (Error Code 4)."""
