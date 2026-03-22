71284ea5eaea2e9554317ed1711639b8d773ee80
daveinc <118027054+daveinc@users.noreply.github.com>
2026-03-22 23:32:59 +0200

    Update __init__.py

--- custom_components/provision_isr/provision_api/__init__.py
+++ custom_components/provision_isr/provision_api/__init__.py
@@ -8,7 +8,7 @@ from .exceptions import (
     PermissionDeniedError,
     ProvisionError,
 )
-from .models import ChannelList, DeviceInfo, DiskInfo, StreamCaps, StreamInfo
+from .models import ChannelList, DeviceInfo, DiskInfo, StreamCaps, StreamInfo, DeviceCapabilities
 from .provision_client import ProvisionClient
 
 __all__ = [
@@ -18,11 +18,13 @@ __all__ = [
     "DiskInfo",
     "StreamCaps",
     "StreamInfo",
+    "DeviceCapabilities",
     "ProvisionError",
     "AuthenticationError",
     "ConnectionError",
     "InvalidRequestError",
     "InvalidXMLFormatError",
+    "InvalidXMLContent",
     "InvalidXMLContentError",
     "PermissionDeniedError",
 ]
